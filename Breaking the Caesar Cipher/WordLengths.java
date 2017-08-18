7cbecc4f11e6d0cb0555489862e0f9d8157958d1
LucyLiu-UCSB <xiliu1991@gmail.com>
2017-08-17 21:09:36 -0700

    Learn array

---
+++ Breaking the Caesar Cipher/WordLengths.java
@@ -0,0 +1,14 @@
+
+/**
+ * figure out the most common word length of words from a file.
+ * 
+ * 
+ * @Lucy
+ */
+import edu.duke.*;
+public class WordLengths {
+    public void countWordLengths(FileResource resource, int[] counts){
+        
+    }
+
+}
