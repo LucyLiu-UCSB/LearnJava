d8e9a1b7721fa778e4a3b1671064e744477c0d22
LucyLiu-UCSB <xiliu1991@gmail.com>
2017-08-29 23:07:35 -0700

    known length

---
+++ VigenereProgram/Tester.java
@@ -0,0 +1,57 @@
+
+/**
+ * Test methods for Vigenere Program
+ * 
+ * @Lucy
+ * 
+ */
+
+import java.util.*;
+import edu.duke.*;
+
+public class Tester {
+    public void testCaesarCipher(){
+        FileResource fr = new FileResource("VigenereTestData/titus-small.txt");
+        String frString=fr.asString();
+        CaesarCipher cc= new CaesarCipher(5);
+        String encrypt=cc.encrypt(frString);
+        String decrypt=cc.decrypt(encrypt);
+        System.out.println(encrypt);
+        System.out.println(decrypt);
+        
+    }
+    
+    public void testCaesarCracker(){
+        FileResource fr = new FileResource("VigenereTestData/oslusiadas_key17.txt");
+        String frString=fr.asString();
+        CaesarCracker cc= new CaesarCracker('a');
+        String decrypt=cc.decrypt(frString);
+        
+        //System.out.println(encrypt);
+        System.out.println(decrypt);
+        
+    }
+    
+    public void testVigenereCracker(){
+        FileResource fr = new FileResource("VigenereTestData/titus-small.txt");
+        String frString=fr.asString();
+        int[] code={17,14,12,4};
+        VigenereCipher cc= new VigenereCipher(code);
+        String encrypt=cc.encrypt(frString);
+        String decrypt=cc.decrypt(encrypt);
+        System.out.println(encrypt);
+        System.out.println(decrypt);
+    }
+    
+    public void testtryKeyLength(){    
+        FileResource fr = new FileResource("VigenereTestData/athens_keyflute.txt");
+        String frString=fr.asString();
+        VigenereBreaker vb= new VigenereBreaker();
+        int[] keys= vb.tryKeyLength(frString,5,'e');
+        for(int i=0;i<keys.length;i++){
+             System.out.println(keys[i]);
+        }      
+    }
+
+
+}
--- VigenereProgram/VigenereBreaker.java
+++ VigenereProgram/VigenereBreaker.java
@@ -3,18 +3,41 @@ import edu.duke.*;
 
 public class VigenereBreaker {
     public String sliceString(String message, int whichSlice, int totalSlices) {
-        //REPLACE WITH YOUR CODE
-        return "WRITE ME!";
+        StringBuilder slice=new StringBuilder();
+        int i=0;
+        char[] sliceChar=message.toCharArray();
+        while (i<message.length()){
+            if (i% totalSlices==whichSlice){
+                slice.append(sliceChar[i]);
+            }
+            i++;
+        
+        }
+        String sliceString=slice.toString();
+        return sliceString;
     }
 
     public int[] tryKeyLength(String encrypted, int klength, char mostCommon) {
         int[] key = new int[klength];
         //WRITE YOUR CODE HERE
+        int i=0;
+        CaesarCracker cc= new CaesarCracker(mostCommon);
+        while ( i < klength){
+            String iPiece=sliceString(encrypted, i,klength);
+            key[i]=cc.getKey(iPiece);
+            i++;
+        }
         return key;
     }
 
     public void breakVigenere () {
         //WRITE YOUR CODE HERE
+        FileResource fr = new FileResource();
+        String frString=fr.asString();
+        int[] keys =tryKeyLength(frString,5,'e');
+        VigenereCipher vc= new VigenereCipher(keys);
+        String res=vc.decrypt(frString);
+        System.out.println(res);
     }
-    
+ 
 }
