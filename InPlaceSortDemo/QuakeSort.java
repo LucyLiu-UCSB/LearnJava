570205da993020b6faf2acedacd3b1ae0999ab1b
LucyLiu-UCSB <xiliu1991@gmail.com>
2017-09-08 23:51:02 -0700

    sort--in place

---
+++ InPlaceSortDemo/QuakeSort.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class QuakeSort {
+    public int getSmallestMagnitude(ArrayList<QuakeEntry> quakes, int from) {
+        int minIdx = from;
+        for (int i = from +1; i < quakes.size(); i++) {
+            if (quakes.get(i).getMagnitude() < quakes.get(minIdx).getMagnitude()) {
+                minIdx = i;
+            }
+        }
+        return minIdx;
+    }
+    
+    public void sortByMagnitude(ArrayList<QuakeEntry> in) {
+        //count from 0 to < in.size()
+        for(int i = 0; i < in.size(); i++) {
+            /* find the index of the smallest quake*/
+            int minIdx = getSmallestMagnitude(in, i);
+            /* swap the ith quake with the minIdxth quake */
+            QuakeEntry qi = in.get(i);
+            QuakeEntry qmin = in.get(minIdx);
+            in.set(i, qmin);
+            in.set(minIdx, qi);
+        }
+    }
+    /* tester method to use in BlueJ */
+    public void testSort(){
+        EarthQuakeParser parser = new EarthQuakeParser();
+        String source = "data/nov20quakedata.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        //sortByMagnitude(list);
+        sortByLargestDepth(list);
+        for(QuakeEntry qe: list) {
+            System.out.println(qe);
+        }
+    }
+    public int getLargestDepth (ArrayList<QuakeEntry> quakeData, int from){
+        int maxInx=from;
+        for (int i=from +1; i<quakeData.size();i++){
+            if(quakeData.get(i).getDepth()>quakeData.get(maxInx).getDepth()){
+                maxInx=i;
+            }
+        }
+        return maxInx;
+    }
+    public void sortByLargestDepth(ArrayList<QuakeEntry>quakeData){
+        for (int i=0;i<quakeData.size();i++){
+            int maxInx=getLargestDepth(quakeData, i);
+            QuakeEntry qi=quakeData.get(i);
+            QuakeEntry qMax=quakeData.get(maxInx);
+            quakeData.set(i,qMax);
+            quakeData.set(maxInx,qi);            
+        }
+    }
+}
